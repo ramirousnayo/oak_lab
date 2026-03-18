b5c80bdcd250d97aa4f382357fe6c859eacc3194
Ramiro Usnayo <rusnayo22@gmail.com>
2026-03-18 10:59:05 -0300

    feat: admin config para Creature, TimeSlot y Booking

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Creature, TimeSlot, Booking
 
-# Register your models here.
+@admin.register(Creature)
+class CreatureAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'emoji']
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ['get_day_display', 'start_time', 'end_time', 'capacity', 'spots_left']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'creature', 'timeslot', 'created_at']
+    ordering = ['-created_at']
